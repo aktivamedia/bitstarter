ce293b173b34ff732aad61162906378f97b8b85a
Ubuntu <ubuntu@ip-172-31-29-101.us-west-2.compute.internal>
2013-07-12 00:11:55 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
+var index_file = fs.readFileSync('index.html')toString();
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Cruel  World 2!');
+  response.send(index_file);
 });
 
 var port = process.env.PORT || 5000;
